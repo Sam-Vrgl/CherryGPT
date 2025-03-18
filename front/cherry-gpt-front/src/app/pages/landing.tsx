50c2b1b691afca4fff839b7616cf386414260c95
Samuel Vergnol <46886716+Sam-Vrgl@users.noreply.github.com>
2025-03-18 16:54:19 +0100

    landing page

---
+++ front/cherry-gpt-front/src/app/pages/landing.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+
+export default function LandingClient() {
+  return (
+    <>
+     <div className="container">
+        <h1>Welcome to Do You Organoid</h1>
+        <p>
+          My mission is to help you discover which organoid solution is best suited
+          to help you with your scientific research.
+        </p>
+        <p>
+          To do that I have analysed over 70 companies around the world building
+          innovative organoids and micro physiological solutions.
+        </p>
+        <p>
+          I will ask you some simple questions and then, if relevant, recommend you
+          the best tools out there.
+        </p>
+        <p>Are you ready to start?</p>
+        <button>Yes</button>
+      </div>
+    </>
+  );
+}
